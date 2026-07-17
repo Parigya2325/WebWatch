5cc235f11741efdd732aec77febc9ed5377bd3f8
Parigya2325 <parigyasingh2710@gmail.com>
2026-07-17 22:23:58 +0530

    Prepare project for Render deployment

--- config.py
+++ config.py
@@ -1,19 +1,8 @@
 import os
 
-class Config:
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://root:merganki27@localhost/webwatch"
+)
 
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:merganki@localhost/webwatch"
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    MAIL_SERVER = "smtp.gmail.com"
-
-    MAIL_PORT = 587
-
-    MAIL_USE_TLS = True
-
-    MAIL_USERNAME = "parigyasingh2710@gmail.com"
-
-    MAIL_PASSWORD = "yxtekuerxngammqb"
-
-    MAIL_DEFAULT_SENDER = "WebWatch <parigyasingh2710@gmail.com>"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
